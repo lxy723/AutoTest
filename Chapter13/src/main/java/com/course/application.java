45de3fcc7cec77aa89c63ee71227716cbae30bcc
lxy723 <1140553684@qq.com>
2018-11-26 11:01:28 +0800

    十三章

---
+++ Chapter13/src/main/java/com/course/application.java
@@ -0,0 +1,17 @@
+package com.course;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+@EnableAutoConfiguration
+public class application {
+    public static void main(String args[]){
+
+        SpringApplication.run(Application.class,args);
+    }
+}
